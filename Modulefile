294eb2dfa812ac398619dd5bea2080892fc547ce
Jon Mosco <jonny.mosco@gmail.com>
2013-09-23 21:51:20 -0400

    template and Modulefile creation

---
+++ Modulefile
@@ -0,0 +1,4 @@
+name 'puppet-tcpd'
+version '0.0.1'
+dependency 'puppetlabs/stdlib', '>=3.2.0'
+description "Manage tcp wrappers"
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,15 @@
 # == class: tcpd
 #
+# == Description
+#
+# Access will be granted when a (daemon,client) pair matches an entry in
+# the /etc/hosts.allow file.
+#
+# Otherwise, access will be denied when a (daemon,client) pair matches an
+# entry in the /etc/hosts.deny file.
+#
+# Otherwise, access will be granted.
+#
 # == Parameters
 #
 # == Authors
