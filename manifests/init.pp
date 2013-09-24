92ced185d2f92cb3782f8b40001428b3b348b053
Jon Mosco <jonny.mosco@gmail.com>
2013-09-24 12:13:41 -0400

    updated templates to start following hosts_allow(5)

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,6 +13,9 @@
 #
 # Otherwise, access will be granted.
 #
+# The format of /etc/{hosts.allow,hosts.deny} should use the following format:
+# daemon_list: client_list [ : shell_command ]
+#
 # == Parameters
 #
 # $default_policy
@@ -29,9 +32,13 @@
 # Jon Mosco <jonny.mosco@gmail.com>
 #
 class tcpd (
-  $default_policy = 'allow',
-  $allowed_hosts  = undef,
-  $denied_hosts   = undef,
+  $default_policy  = 'allow',
+  $allowed_domains = undef,
+  $allowed_hosts   = undef,
+  $denied_domains  = undef,
+  $denied_hosts    = undef,
+  $daemon          = undef,
+  $policy          = undef,
 ) {
 
   if $default_policy == 'allow' {
@@ -41,9 +48,14 @@ class tcpd (
     $deny = 'ALL: ALL'
   }
 
-  validate_re($default_policy, '^(allow|deny)$', "Valid values for \$default_policy are 'allow', 'deny'. Received ${default_policy}")
+  validate_re($default_policy, '^(allow|deny)$', "Valid values for
+  \$default_policy are 'allow', or 'deny'. Received ${default_policy}")
   validate_string($allow)
   validate_string($deny)
+  validate_string($allowed_domains)
+  validate_string($allowed_hosts)
+  validate_string($denied_domains)
+  validate_string($denied_hosts)
 
   file { 'hosts.allow':
     ensure  => file,
